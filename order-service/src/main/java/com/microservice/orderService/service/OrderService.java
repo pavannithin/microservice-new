10dbd04e0ec8291db2fb6907d4ac20b8f4d381b4
pavannithin <nithin.pavan@gmail.com>
2024-07-12 01:03:18 +0530

    validating if all the products are aviable in inventory

--- order-service/src/main/java/com/microservice/orderService/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderService/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservice.orderService.service;
 
+import com.microservice.orderService.dto.InventoryResponse;
 import com.microservice.orderService.dto.OrderLineItemsDto;
 import com.microservice.orderService.dto.OrderRequest;
 import com.microservice.orderService.model.Order;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,15 +25,22 @@ public class OrderService {
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItems().stream().map(this::mapToDto).toList();
         order.setOrderLineItems(orderLineItems);
-/*
-Before placing order  willi will check if all the
- products are present by calling inventory service API
-  using web client(webclient supports both synchronous
-  and asynchronous calls then rest template)
-*/
-        /*body to mono method takes the return type of the API*/
-        Boolean isOrder = webClient.get().uri("hhttp://localhost:8082/api/inventory").retrieve().bodyToMono(boolean.class).block();
-        if (isOrder) {
+        List<String> orderSKUCodesList = order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode).toList();
+        /*
+        Before placing order  willi will check if all the
+         products are present by calling inventory service API
+          using web client(webclient supports both synchronous
+          and asynchronous calls then rest template)
+        */
+
+        /* Body to mono method takes the return type of the API */
+
+        InventoryResponse[] inventoryResponses = webClient.get().uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", orderSKUCodesList).build())
+                .retrieve().bodyToMono(InventoryResponse[].class)
+                .block();
+        boolean isAllProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::getIsInStock);
+        if (isAllProductsInStock) {
             orderRepository.save(order);
         } else {
             throw new IllegalArgumentException("Product is not in the stock, please try again later");
