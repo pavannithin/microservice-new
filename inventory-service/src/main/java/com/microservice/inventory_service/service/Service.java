0c5009e86d74760f93af0f5e6329332e11f44b37
pavannithin <nithin.pavan@gmail.com>
2024-07-11 23:21:14 +0530

    modified the api to  support for multiple products inventory data response

--- inventory-service/src/main/java/com/microservice/inventory_service/service/Service.java
+++ inventory-service/src/main/java/com/microservice/inventory_service/service/Service.java
@@ -1,8 +1,10 @@
 package com.microservice.inventory_service.service;
 
+import com.microservice.inventory_service.dto.InventoryResponse;
 import com.microservice.inventory_service.model.Inventory;
 import com.microservice.inventory_service.repos.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +12,13 @@ import java.util.Optional;
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class Service {
-private final InventoryRepository inventoryRepository;
-    public boolean isInStock(String skuCode) {
-        Optional<Inventory> allBySkuCode = inventoryRepository.findAllBySkuCode(skuCode);
-        return allBySkuCode.isPresent();
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        List<Inventory> allBySkuCode = inventoryRepository.findBySkuCodeIn(skuCode);
+        List<InventoryResponse> inventoryResponses = allBySkuCode.stream().map(eachInventory -> InventoryResponse.builder()
+                .skuCode(eachInventory.getSkuCode()).isInStock(eachInventory.getStock() > 0).build()).toList();
+        return inventoryResponses;
     }
 }
